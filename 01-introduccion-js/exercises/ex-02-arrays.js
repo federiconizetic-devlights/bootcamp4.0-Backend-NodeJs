f33f2b48434428275cd268e99558829b2abc8d5f
Federico <federico.nizetic@devlights.com>
2025-10-07 12:50:19 -0300

    fixing tests

--- 01-introduccion-js/exercises/ex-02-arrays.js
+++ 01-introduccion-js/exercises/ex-02-arrays.js
@@ -2,7 +2,7 @@
 
 
 1. Crea un array con 5 comidas favoritas.
-2. Agrega una nueva comida al final.
+2. Agrega una nueva comida al final. (asado)
 3. Elimina la primera comida.
 4. Muestra el array resultante y su longitud.
 
--- 01-introduccion-js/exercises/tests/ex-02-arrays.test.js
+++ 01-introduccion-js/exercises/tests/ex-02-arrays.test.js
@@ -1,8 +1,11 @@
 import { modificarComidas } from "../ex-02-arrays.js";
 
 test('modificarComidas agrega y quita correctamente', () => {
-  const inicio = ['pizza','empanada','milanesa'];
+
+  const inicio = ['pizza','empanada','milanesa','tarta','hamburguesa'];
   const result = modificarComidas(inicio);
+
+  expect(result).not.toContain('pizza');
   expect(result).toContain('asado');
-  expect(result.length).toBe(3);
+  expect(result.length).toBe(inicio.length);
 });
